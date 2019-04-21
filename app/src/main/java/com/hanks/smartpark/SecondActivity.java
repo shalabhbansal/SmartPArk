f37aa6b83c3612cda20a59f81d1b9d6efa879739
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2019-04-22 00:45:00 +0530

    Add Display Screen

--- app/src/main/java/com/hanks/smartpark/SecondActivity.java
+++ app/src/main/java/com/hanks/smartpark/SecondActivity.java
@@ -1,7 +1,10 @@
 package com.hanks.smartpark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SecondActivity.this, ActivityDisplay.class);
+                startActivity(i);
+            }
+        });
     }
 }
